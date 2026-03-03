b0ba0f31a678e44d49ea62c7723fb4af2027bd41
ashanti-cocroft <ashanticocroft23@gmail.com>
2026-03-03 17:40:30 -0600

    fix api fallback URL to railway

--- FRONTEND/frontend/lib/api.js
+++ FRONTEND/frontend/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
   ? `${process.env.NEXT_PUBLIC_API_URL}/api/`
-  : "http://127.0.0.1:8000/api/";
+  : "https://headzup-barbershop-website-production.up.railway.app/api/";
 
 const API = axios.create({
   baseURL: BASE_URL,
