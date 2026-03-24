5458e98966c46af467ceffbeab3f63e1e0ffe1d3
ashanti-cocroft <ashanticocroft23@gmail.com>
2026-03-23 19:11:08 -0500

    fix CSRF trusted origins for Django admin

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -11,6 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://headzup-barbershop-website-production.up.railway.app",
+    "https://headzup-barbershop-website.vercel.app",
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
