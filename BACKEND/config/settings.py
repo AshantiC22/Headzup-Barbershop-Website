6734d85a891433b0377d4276fec2e0ff7496f0e2
ashanti-cocroft <ashanticocroft23@gmail.com>
2026-03-24 22:56:23 -0500

    fix CORS for barber register endpoint

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -113,6 +113,28 @@ CORS_ALLOWED_ORIGINS = [
     "https://headzup-barbershop-website.vercel.app",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 STRIPE_SECRET_KEY      = os.environ.get("STRIPE_SECRET_KEY", "")
 STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY", "")
 
