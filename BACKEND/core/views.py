887fcd8b35d5b9b3757543e7b87ed0edc9b3b5d5
ashanti-cocroft <ashanticocroft23@gmail.com>
2026-04-03 06:04:03 -0500

    update security questions - 5 easy personal questions

--- BACKEND/core/views.py
+++ BACKEND/core/views.py
@@ -674,14 +674,11 @@ class CheckUsernameView(APIView):
 
 
 SECURITY_QUESTIONS = [
-    "What was the name of your first pet?",
-    "What city were you born in?",
-    "What is your mother's maiden name?",
-    "What was the name of your elementary school?",
     "What was your childhood nickname?",
-    "What is the name of the street you grew up on?",
+    "What is your favorite food?",
+    "What is your favorite color?",
     "What was the make of your first car?",
-    "What is your oldest sibling's middle name?",
+    "What is your mother's last name?",
 ]
 
 
