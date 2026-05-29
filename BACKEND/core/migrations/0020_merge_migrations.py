c71b4f6f367dedf4f71beccf4aaacf41f6a41a18
ashanti-cocroft <ashanticocroft23@gmail.com>
2026-05-29 06:35:06 -0500

    fix migration conflict - merge 0016 and 0019

---
+++ BACKEND/core/migrations/0020_merge_migrations.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0016_review_barber_seen"),
+        ("core", "0019_review_barber_reply"),
+    ]
+
+    operations = [
+    ]
